4cada8137d361fec680aa02ec212374a15c81772
Sreejeeta Dutta <141065240+Sreejeeta-99@users.noreply.github.com>
2024-08-14 23:22:19 +0530

    Delete Week_3_Exercises directory

--- Week_3_Exercises/Exercise 3/EmployeeManagementSystem/src/main/java/com/code/employee/repository/DepartmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.code.employee.repository;
-
-import com.code.employee.entity.Department;
-
-public interface DepartmentRepository {
-	Department findByName(String name);
-}
--- Week_3_Exercises/Exercise 4/EmployeeManagementSystem/src/main/java/com/code/employee/repository/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.code.employee.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.code.employee.entity.Department;
-
-public interface DepartmentRepository extends JpaRepository<Department,Integer>{
-	Department findByName(String name);
-}
